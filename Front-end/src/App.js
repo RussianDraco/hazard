8d9bb3792c6fa4c5b140adb8adcc19882faf433f
Sandro Shtikov <64151238+AirWard1234@users.noreply.github.com>
2024-09-14 13:01:48 -0400

    push 4 i think
    working routes + about section

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -2,14 +2,19 @@ import About from "./components/About";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Sponsor from "./components/Sponsor";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from "../src/pages/Home"
 
 function App() {
   return (
     <>
       <Navbar />
-      <Hero />
-      <Sponsor />
-      <About />
+      <Router>
+        <Routes>
+          <Route path="/" element = {<><Hero/><Sponsor/><About/></>} />
+          <Route path="/login" element = {<h1 className="text-9xl">1</h1>} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- Front-end/src/components/Navbar.jsx
+++ Front-end/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav className='fixed z-10 w-full h-20 shadow-md flex justify-evenly items-center backdrop-filter backdrop-blur-md '>
-        <div className="text-3xl font-medium">
+          <a href='/' ><div className="text-3xl font-medium">
             HAZARD
-        </div>
+          </div></a>
         <div className="text-black/65 flex justify-evenly w-[20%] ">
             <a className='hover:text-black hover:cursor-pointer transition'>About</a>
             <a className='hover:text-black hover:cursor-pointer transition'>Devlopers</a>
@@ -13,7 +14,7 @@ const Navbar = () => {
             <a className='hover:text-black hover:cursor-pointer transition'>HTN2024</a>
         </div>
         <div className="px-3 py-2 border-none shadow-md hover:scale-110 hover:shadow-lg transition rounded-lg bg-gradient-to-r from-orange-400 to-orange-600">
-            <button>Install Now</button>
+          <a href='/login'><button type='button'>Install Now</button></a>
         </div>
 
     </nav>
