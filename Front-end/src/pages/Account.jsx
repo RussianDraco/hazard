3d7b895d640cf542993eb679075a48414678b344
Sandro Shtikov <64151238+AirWard1234@users.noreply.github.com>
2024-09-14 20:29:55 -0400

    account

---
+++ Front-end/src/pages/Account.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Account = () => {
+  return (
+    <>
+    <div className="flex justify-center translate-y-36">
+        <div className="w-[40vw] h-[60vh] rounded-3xl  shadow-xl"></div>
+
+    </div>
+    </>
+  )
+}
+
+export default Account
